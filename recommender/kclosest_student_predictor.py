309f1130f01b27c129339845d45ed3a4bc2b8f55
Srinivas Avireddy <sriniavireddy@gmail.com>
2016-04-23 03:02:09 -0700

    added k closest student predictor

--- recommender/kclosest_student_predictor.py
+++ recommender/kclosest_student_predictor.py
@@ -1,1 +1,8 @@
-__author__ = 'maheshwar'
+__author__ = 'Srinivas Avireddy'
+import pandas as pd
+
+def find_mean_vector(student_id):
+    data = pd.read_csv("../data/data.csv")
+    data_student =  data.loc[data['student_id'] == int(student_id)]
+    print data_student
+find_mean_vector(2)
