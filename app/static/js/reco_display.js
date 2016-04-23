a4f77b6cd10e5afa05d0976fba4b966cbf31b927
joemanley201 <joemanley201@gmail.com>
2016-04-23 12:28:37 -0700

    Commit changes for charts

--- app/static/js/reco_display.js
+++ app/static/js/reco_display.js
@@ -1,28 +1,93 @@
-function populateAnalysisTable() {
-    var table = $("#analysisTable")
+var courses = [];
 
-    html = ""
-    for (var i = 0, course_length = response.course_list.length; i < course_length; i++) {
-        html += '<h2>' + response.course_list[i] + '</h2>';
-        for (var j = 0, metric_length = 11; j < metric_length; j++) {
-            html += '<div class="row"><div class="col-md-4">' + response.metric_list[(i * 11) + j] + '</div><div class="col-md-4"><span class="glyphicon glyphicon-chevron-up success" aria-hidden="true"></span>' + response.increase_list[(i * 11) + j] + '</div></div>'
-        }
-    }
-    html += '<div class="row"><div class="col-md-4">Hi</div><div class="col-md-4">Hello</div></div>'
-    table.append(html)
+function zipper(metrics, currentScores, targetScores) {
+  var response = [];
+  response.push(["Metric", "Current", "Target"])
+  for (var idx = 0, lengthh = metrics.length; idx < lengthh; idx++) {
+    response.push([metrics[idx], currentScores[idx], targetScores[idx]])
+  }
+  return response;
 }
+
+function populateTable() {
+  var tableDiv = $("#analysisTable");
+  html = ""
+  for (var idx = 0, lengthh = courses.length; idx < lengthh; idx++) {
+    html += '<tr><td class="col-lg-8"><div id="chart_' + idx + '"></div></td></tr>'
+  }
+  tableDiv.append(html);
+}
+
+function populateResponse(response) {
+  for (var i = 0, course_length = response.course_list.length; i < course_length; i++) {
+    var currData = {};
+    currData.closestGrades = response.closest_grades_list[i];
+    currData.currentScores = response.current_student_list[i];
+    currData.targetScores = response.original_list[i];
+    currData.metrics = response.metric_list[i];
+    currData.courseName = response.course_list[i];
+    courses.push(currData);
+  }
+  console.log(courses);
+
+}
+
+function getData(idx) {
+  var course = courses[idx];
+  var data = zipper(course.metrics, course.currentScores, course.targetScores);
+  return data;
+}
+
+function drawChart(courseName, placeholder) {
+  var data = google.visualization.arrayToDataTable(getData(0));
+
+  var options = {
+    width: 900,
+    height: 500,
+    chart: {
+      title: 'Comparison for Course ' + courseName
+    },
+    hAxis: {
+      title: ''
+    },
+    vAxis: {
+      title: 'Metrics',
+      minValue: 0,
+      maxValue: 100
+    },
+    bars: 'horizontal'
+  };
+  var material = new google.charts.Bar(document.getElementById(placeholder));
+  material.draw(data, options);
+}
+
 $(document).ready(function() {
-    $('#test1').bind('click', function() {
-      $.ajax({
-        url: '/testAjax',
-            type: 'POST',
-            data: [1,2,3,4,5],
-            contentType: 'application/json;charset=UTF-8',
-            success: function(evt) {
-                alert(evt.data);
-            }
-        });
-      return false;
+  populateResponse(response);
+  populateTable();
+  google.charts.load('current', {
+    packages: ['corechart', 'bar']
+  });
+  for (var idx = 0, lengthh = courses.length; idx < lengthh; idx++) {
+    var data = courses;
+    (function(idx) {
+      google.charts.setOnLoadCallback(function() {
+        drawChart(data[idx].courseName, "chart_" + (idx));
+      });
+    })(idx);
+  }
+
+
+  $('#test1').bind('click', function() {
+    $.ajax({
+      url: '/testAjax',
+      type: 'POST',
+      data: [1, 2, 3, 4, 5],
+      contentType: 'application/json;charset=UTF-8',
+      success: function(evt) {
+        alert(evt.data);
+      }
     });
+    return false;
+  });
 
 });
