833581f3e2addef4ef0514ce788b283c30ee0b02
ramkishore31 <ramkishore31@gmail.com>
2016-04-23 02:59:19 -0700

    Visualizing recommendation v3

--- app/app.py
+++ app/app.py
@@ -38,64 +38,51 @@ def generate_recommendation():
 
     data_student =  data.loc[data['student_id'] == int(student_id)]
     for i in range(len(course_list)):
-        print str(course_list[i])
         data_courselist = data[(data.course_id == str(course_list[i])) & (data.grade == grade[i])]
         print 'Your scores need to increase by the following %'
         if data_courselist['class_participation'].mean() > data_student['class_participation'].mean():
             metric_list.append('Class Participation')
             increase_list.append(data_courselist['class_participation'].mean() - data_student['class_participation'].mean())
-            print 'Class Participation',data_courselist['class_participation'].mean() - data_student['class_participation'].mean()
 
         if data_courselist['attendance'].mean() > data_student['attendance'].mean():
-            print 'Attendance',data_courselist['attendance'].mean() - data_student['attendance'].mean()
             metric_list.append('Attendance')
             increase_list.append(data_courselist['attendance'].mean() - data_student['attendance'].mean())
 
         if data_courselist['calibration'].mean() > data_student['calibration'].mean():
-            print 'Calibration',data_courselist['calibration'].mean() - data_student['calibration'].mean()
             metric_list.append('Calibration')
             increase_list.append(data_courselist['calibration'].mean() - data_student['calibration'].mean())
 
         if data_courselist['assignments'].mean() > data_student['assignments'].mean():
-            print 'Assignments',data_courselist['assignments'].mean() - data_student['assignments'].mean()
             metric_list.append('Assignments')
             increase_list.append(data_courselist['assignments'].mean() - data_student['assignments'].mean())
 
         if data_courselist['midterm'].mean() > data_student['midterm'].mean():
-            print 'Midterm',data_courselist['midterm'].mean() - data_student['midterm'].mean()
             metric_list.append('Midterm')
             increase_list.append(data_courselist['midterm'].mean() - data_student['midterm'].mean())
 
         if data_courselist['final'].mean() > data_student['final'].mean():
-            print 'Final',data_courselist['final'].mean() - data_student['final'].mean()
             metric_list.append('Final')
             increase_list.append(data_courselist['final'].mean() - data_student['final'].mean())
 
         if data_courselist['weekly_hours_1'].mean() > data_student['weekly_hours_1'].mean():
-            print 'Weekly hours 1',data_courselist['weekly_hours_1'].mean() - data_student['weekly_hours_1'].mean()
             metric_list.append('Weekly hours 1')
             increase_list.append(data_courselist['weekly_hours_1'].mean() - data_student['weekly_hours_1'].mean())
 
         if data_courselist['weekly_hours_2'].mean() > data_student['weekly_hours_2'].mean():
-            print 'Weekly hours 2',data_courselist['weekly_hours_2'].mean() - data_student['weekly_hours_2'].mean()
             metric_list.append('Weekly hours 2')
             increase_list.append(data_courselist['weekly_hours_2'].mean() - data_student['weekly_hours_2'].mean())
 
         if data_courselist['weekly_hours_3'].mean() > data_student['weekly_hours_3'].mean():
-            print 'Weekly hours 3',data_courselist['weekly_hours_3'].mean() - data_student['weekly_hours_3'].mean()
             metric_list.append('Weekly hours 3')
             increase_list.append(data_courselist['weekly_hours_3'].mean() - data_student['weekly_hours_3'].mean())
 
         if data_courselist['weekly_hours_4'].mean() > data_student['weekly_hours_4'].mean():
-            print 'Weekly hours 4',data_courselist['weekly_hours_4'].mean() - data_student['weekly_hours_4'].mean()
             metric_list.append('Weekly hours 4')
             increase_list.append(data_courselist['weekly_hours_4'].mean() - data_student['weekly_hours_4'].mean())
 
         if data_courselist['weekly_hours_5'].mean() > data_student['weekly_hours_5'].mean():
-            print 'Weekly hours 5',data_courselist['weekly_hours_5'].mean() - data_student['weekly_hours_5'].mean()
             metric_list.append('Weekly hours 5')
             increase_list.append(data_courselist['weekly_hours_5'].mean() - data_student['weekly_hours_5'].mean())
-        print "\n"
 
     return render_template('reco_display.html',metrics = metric_list,change = increase_list,course = course_list)
 if __name__ == '__main__':
